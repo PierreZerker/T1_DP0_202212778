3045ac9dffa005a189b24c064b03e860bd443c52
F3rm4t <20jarnulfo@gmail.com>
2023-08-17 22:22:13 -0500

    Taller1 - Probado

---
+++ src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,19 @@
+package uniandes.dpoo.taller0.modificacion;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class Modificacion {
+	/*
+	 * La función carga el archivo y busca el pais mas medallistas.
+	 */
+	public static void main(String[] args) {
+		try {
+			CalculadoraEstadisticas calc = LoaderOlimpicos
+					.cargarArchivo("C:/Users/arnul/Documents/Taller1_Olimpicos/data/atletas.csv");
+			System.out.println(calc.paisConMasMedallistas());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
